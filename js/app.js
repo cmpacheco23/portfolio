0dae1690d8514ede2ac5a0145d0284fc3560c2ff
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-01 22:13:22 -0400

    add event listener for experience and add conditional rendering

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+const experienceDivs = document.querySelectorAll('.experience');
+
+experienceDivs.forEach((div) => {
+  const description = div.querySelector('.description');
+  const arrowIcon = div.querySelector('.arrow-icon');
+
+  // Initialize the description to be hidden and the down arrow visible
+  description.style.display = 'none';
+  arrowIcon.src = '/images/down-arrow.png';
+
+  div.addEventListener('click', () => {
+    if (description.style.display === 'none') {
+      description.style.display = 'block';
+      arrowIcon.src = '/images/up-arrow.png'; // Change to the up arrow image
+    } else {
+      description.style.display = 'none';
+      arrowIcon.src = '/images/down-arrow.png'; // Change to the down arrow image
+    }
+  });
+});
